94994c5344f1638d88cd01fa9d71b1e0e02d3bca
Marta Rodriguez <marta.rodriguezorejuela@infarm.com>
2021-03-08 09:23:56 +0100

    Apollo client

--- src/app.tsx
+++ src/app.tsx
@@ -7,7 +7,14 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import { TopHeader } from "./components/index";
 import { Home, Area } from "./routes/index"
+import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client'
 
+const client = new ApolloClient({
+    link: createHttpLink({
+        uri: 'http://127.0.0.1:8000/graphiql'
+    }),
+    cache: new InMemoryCache(),
+});
 
 function App() {
     return (
@@ -15,15 +22,19 @@ function App() {
             <ThemeProvider theme={theme}>
                 <GlobalStyles />
                 <TopHeader />
-                <Router>
-                    <Switch>
-                        <Route path="/" exact component={() => <Home />} />
-                        <Route path="/area/:areaId" exact component={() => <Area />} />
+                <ApolloProvider client={client}>
+                    <Router>
+                        <Switch>
+                            <Route path="/" exact component={() => <Home />} />
+                            <Route path="/area/:areaId" exact component={() => <Area />} />
 
-                    </Switch>
-                </Router>
+                        </Switch>
+                    </Router>
+                </ApolloProvider>
             </ThemeProvider>
         </div>
+
+
     );
 }
 
